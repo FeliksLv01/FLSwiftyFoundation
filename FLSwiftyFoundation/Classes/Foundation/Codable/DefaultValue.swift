0665c5cbf3e7c898d1d1d6c637163d2b03caf79c
felikslv <felikslv@163.com>
2022-08-15 14:32:18 +0800

    refactor: DefaultValue

--- FLSwiftyFoundation/Classes/Foundation/Codable/DefaultValue.swift
+++ FLSwiftyFoundation/Classes/Foundation/Codable/DefaultValue.swift
@@ -6,54 +6,75 @@
 //
 
 import Foundation
-import UIKit
-
 
 public protocol DefaultValue {
-    associatedtype CodableValue: Codable
-    static func defaultValue() -> CodableValue
+    static var defaultValue: Self { get }
 }
 
+public typealias DefaultCodableValue = DefaultValue & Codable
+
 @propertyWrapper
-public struct Default<T: DefaultValue>: Codable {
-    public var wrappedValue: T.CodableValue
+public struct Default<T: DefaultCodableValue>: Codable {
+    public var wrappedValue: T
     
-    public init(wrappedValue: T.CodableValue) {
+    public init(wrappedValue: T) {
         self.wrappedValue = wrappedValue
     }
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        wrappedValue = (try? container.decode(T.CodableValue.self)) ?? T.defaultValue()
+        wrappedValue = (try? container.decode(T.self)) ?? T.defaultValue
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(wrappedValue)
     }
 }
 
+// 对于 key 不存在或者意外输入不同类型时使用默认的值
 public extension KeyedDecodingContainer {
     func decode<T>(
         _ type: Default<T>.Type,
         forKey key: Key
     ) throws -> Default<T> where T: DefaultValue {
-        try decodeIfPresent(type, forKey: key) ?? Default(wrappedValue: T.defaultValue())
+        try decodeIfPresent(type, forKey: key) ?? Default(wrappedValue: T.defaultValue)
     }
 }
 
+// 数组相关的处理，含义和 KeyedDecodingContainer 的处理一样
+public extension UnkeyedDecodingContainer {
+    mutating func decode<T>(
+        _ type: Default<T>.Type
+    ) throws -> Default<T> where T : DefaultCodableValue {
+            try decodeIfPresent(type) ?? Default(wrappedValue: T.defaultValue)
+    }
+}
 
 extension Bool: DefaultValue {
-    public static func defaultValue() -> Bool { false }
+    public static var defaultValue: Bool = false
 }
 
 extension Int: DefaultValue {
-    public static func defaultValue() -> Int { 0 }
+    public static var defaultValue: Int = 0
 }
 
 extension String: DefaultValue {
-    public static func defaultValue() -> String { "" }
+    public static var defaultValue: String = ""
 }
 
-extension Array: DefaultValue where Element: Codable {
-    public static func defaultValue() -> Array<Element> { [] }
+extension Double: DefaultValue {
+    public static var defaultValue = 0.0
 }
 
-extension Dictionary: DefaultValue where Key: Codable, Value: Codable {
-    public static func defaultValue() -> Dictionary<Key, Value> { [:] }
+extension Float: DefaultValue {
+    public static var defaultValue: Float = 0.0
+}
+
+extension Array: DefaultValue {
+    public static var defaultValue: Array<Element> { [] }
+}
+
+extension Dictionary: DefaultValue {
+    public static var defaultValue: Dictionary<Key, Value> { [:] }
 }
